fbca54a4ca2706b01c0576c499bb695031ba16f5
Novoda <cora@novoda.com>
2019-02-05 14:04:03 +0000

    So far code does: Create array for each cell/row Fill array with data Configure checkmark to be the same (prototype and in code (array) match) (When asked by table view) Provides 	-number of rows (no longer hard coded) 	-Specific cell (label look and text, data) Action when cell tapped

--- IOSApprentice/ChecklistViewController.swift
+++ IOSApprentice/ChecklistViewController.swift
@@ -7,11 +7,10 @@
 //
 
 import UIKit
-var items = [ChecklistItem]()
 
 class ChecklistViewController: UITableViewController {
+    var items = [ChecklistItem]()
    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -32,52 +31,52 @@ class ChecklistViewController: UITableViewController {
         let item4 = ChecklistItem()
         item4.text = "Soccer practice"
         items.append(item4)
+        
         let item5 = ChecklistItem()
         item5.text = "Eat ice cream"
         items.append(item5)
+        
+        let item6 = ChecklistItem()
+        item6.text = "Test"
+        items.append(item6)
     }
     
-    func configureCheckmark(for cell: UITableViewCell, at indexPath: IndexPath) {
-        let item = items[indexPath.row]
+    func configureCheckmark(for cell: UITableViewCell, with item: ChecklistItem) {
         if item.checked {
             cell.accessoryType = .checkmark
         } else {
             cell.accessoryType = .none
         }
     }
+    
+    func configureText(for cell: UITableViewCell, with item: ChecklistItem) {
+        let label = cell.viewWithTag(1000) as! UILabel
+        label.text = item.text
+        //the label (which is this one 1000 in the prototype) in that row, will be as below
+    }
 
     // MARK:- Table View Data Source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return items.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //for this table view "" (input), what is the cell for this row "e.g. 5" (input)
-        
-       
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
         //for row (input) the cell is the table view cell 'checklistItem'
-        
         let item = items[indexPath.row]
-        
-        //the label (which is this one 1000 in the prototype) in that row, will be as below
-        let label = cell.viewWithTag(1000) as! UILabel
-        
-        label.text = item.text
-        configureCheckmark(for: cell, at: indexPath)
+    
+        configureCheckmark(for: cell, with: item)
+        configureText(for: cell, with: item)
         return cell
     }
     
     // MARK:- Table View Delegate
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {//method called when tap- what happens (e.g 5th) row
-       
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {//method called when tap- what happens (e.g 5th) row.... if tapped then convert it to the other
         if let cell = tableView.cellForRow(at: indexPath) {
-            
-        
-            // if tapped then convert it to the other
             let item = items[indexPath.row]
-            item.checked = !item.checked
-            configureCheckmark(for: cell, at: indexPath)
+            item.toggleChecked()
+            configureCheckmark(for: cell, with: item)
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
