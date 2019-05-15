0023805978ba1bf9370f201c6835a240520de4f3
Cora-novoda <cora@novoda.com>
2019-05-15 09:53:50 +0100

    Making Add item view controller

---
+++ IOSApprentice/AddItemViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddItemViewController.swift
+//  IOSApprentice
+//
+//  Created by Novoda on 14/05/2019.
+//  Copyright © 2019 cjm. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.largeTitleDisplayMode = .never
+    }
+}
